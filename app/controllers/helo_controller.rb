d2b035a9392355ae2e435c88981b0ac9e03fc2bd
tmatsugi <tmatsugi@pasonatech.co.jp>
2017-08-07 06:51:33 +0000

    コントローラーから値をもらう

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -1,34 +1,11 @@
 class HeloController < ApplicationController # ApplicationControllerを継承したHeloControllerである
     # defはメソッド宣言
     # htt://ドメイン/○○/aaa -> ○○のaaaを実行
-    
+    # 何も書かなかった場合はデフォルトに順ずる
     def index
-        # render plain:"Hello, This is Rails sample page!"
-        
-       # msg = '
-        #   <html>
-        #      <body>
-        #           <h1>sample page</h1>
-        #           <p>this is Rails sample page!</p>
-        #      </body>
-        #   </html>
-        #
-        #render html: msg.html_safe       
-        
-        #if params['msg'] != nil then
-        #    msg = 'Hello, ' + params['msg'] + '!'
-        #else
-        #    msg = 'This is Rails sample page!'
-        #end
-        #msg = '
-        #    <html>
-        #        <body>
-        #            <h1>sample page</h1>
-        #            <p>' + msg + '</p>
-        #        </body>
-        #    </html>
-        #    '
-        #render html: msg.html_safe  
+        # render index.html.erbを書かなくても移動する
+        @title = "Viewサンプル"
+        @msg = "コントローラーで用意した値です"
     end
 
 end
