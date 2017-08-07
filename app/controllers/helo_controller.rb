1361b381d9bfe6d0d6f09d7f9d7972812fbf1f1b
tmatsugi <tmatsugi@pasonatech.co.jp>
2017-08-07 06:43:54 +0000

    Viewの追加

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -5,15 +5,30 @@ class HeloController < ApplicationController # ApplicationControllerを継承し
     def index
         # render plain:"Hello, This is Rails sample page!"
         
-        msg = '
-            <html>
-                <body>
-                    <h1>sample page</h1>
-                    <p>this is Rails sample page!</p>
-                </body>
-            </html>
-            '
-        render html: msg.html_safe       
+       # msg = '
+        #   <html>
+        #      <body>
+        #           <h1>sample page</h1>
+        #           <p>this is Rails sample page!</p>
+        #      </body>
+        #   </html>
+        #
+        #render html: msg.html_safe       
+        
+        #if params['msg'] != nil then
+        #    msg = 'Hello, ' + params['msg'] + '!'
+        #else
+        #    msg = 'This is Rails sample page!'
+        #end
+        #msg = '
+        #    <html>
+        #        <body>
+        #            <h1>sample page</h1>
+        #            <p>' + msg + '</p>
+        #        </body>
+        #    </html>
+        #    '
+        #render html: msg.html_safe  
     end
 
 end
