fdcb2a44c569a04d1c2dccd0c924162fb5fe1478
tmatsugi <tmatsugi@pasonatech.co.jp>
2017-08-07 06:05:55 +0000

    controller add html

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -3,7 +3,17 @@ class HeloController < ApplicationController # ApplicationControllerを継承し
     # htt://ドメイン/○○/aaa -> ○○のaaaを実行
     
     def index
-        render plain:"Hello, This is Rails sample page!"
+        # render plain:"Hello, This is Rails sample page!"
+        
+        msg = '
+            <html>
+                <body>
+                    <h1>sample page</h1>
+                    <p>this is Rails sample page!</p>
+                </body>
+            </html>
+            '
+        render html: msg.html_safe       
     end
 
 end
