116a8054c058cc7d23e2c3fb8a6cd2893afe8743
tmatsugi <tmatsugi@pasonatech.co.jp>
2017-08-08 01:25:52 +0000

    セレクトボックスその２

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -9,7 +9,10 @@ class HeloController < ApplicationController # ApplicationControllerを継承し
         if request.post? then
             @title = 'Result'
             if params['s1'] then
-                @msg = 'you selected:' + params['s1']
+                @msg = 'you selected: '
+                for val in params['s1']
+                    @msg += val + ''
+                end
             else
                 @msg = 'not selected...'
             end
