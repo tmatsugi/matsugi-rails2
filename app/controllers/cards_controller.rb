628e8b203dc6bf2d98e5a36d215771c0b1630a8f
tmatsugi <tmatsugi@pasonatech.co.jp>
2017-08-09 06:47:38 +0000

    各種機能の追加

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,14 +1,52 @@
 class CardsController < ApplicationController
-  
-  def index
-  end
+	layout 'cards'
 
-  def show
-  end
+	def index
+		@cards = Card.all
+	end
 
-  def add
-  end
 
-  def edit
-  end
+	def show
+		@card = Card.find(params[:id])
+	end
+
+
+	def add
+		if request.post? then
+			Card.create(card_params)
+			goback
+		else
+			@card = Card.new
+		end
+	end
+
+
+	def edit
+		@card = Card.find(params[:id])
+		if request.patch? then
+			@card.update(card_params)
+			goback
+		end
+	end
+
+
+	def delete
+		Card.find(params[:id]).destroy
+		goback
+	end
+
+
+	private
+	def card_params
+		params.require(:card).permit(:title, :author, :price, :publisher, :memo)
+	end
+
+
+	private
+	def goback
+		redirect_to '/cards'
+	end
+
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
     get 'cards/index'
-
-    get 'cards/show'
+    get '/cards/', to: 'cards#index'
 
     get 'cards/add'
+    post 'cards/add'
 
-    get 'cards/edit'
+    get 'cards/edit/:id', to: 'cards#edit'
+    patch 'cards/edit/:id', to: 'cards#edit'
+
+    get 'cards/:id', to: 'cards#show'
+    
+    get 'card/delete/:id', to: 'cards#delete'
 
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     # get 'コントローラー/アクション'
