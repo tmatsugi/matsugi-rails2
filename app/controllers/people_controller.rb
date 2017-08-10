fd43100c83c75aee455ab61e58e502dc8a97ccb1
tmatsugi <tmatsugi@pasonatech.co.jp>
2017-08-10 02:19:41 +0000

    バリテーションその２

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -24,14 +24,19 @@ class PeopleController < ApplicationController
     def create
         @person = Person.new person_params
        	if @person.save then
-  		    redirect_to '/people'
+  		      redirect_to '/people'
         else
-          @msg = '入力に問題があります。'
-          render 'add'
+            re = ''
+            @person.errors.messages.each do |key, vals|
+                vals.each do |val|
+                    re += '<span style="color:red">' + key.to_s + '</span> ' + val + '<br>'
+                end
+            end
+            @msg = re.html_safe
+            render 'add'
         end
     end
 
-    
     def edit
         @msg = "edit data.[id = " + params[:id] + "]"
         @person = Person.find(params[:id])
