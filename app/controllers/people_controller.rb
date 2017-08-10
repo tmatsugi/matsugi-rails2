6ae91611716a4258c4da90207602cf06d13f2f84
tmatsugi <tmatsugi@pasonatech.co.jp>
2017-08-10 00:49:06 +0000

    プレースフォルダー

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -49,7 +49,7 @@ class PeopleController < ApplicationController
 	@msg = 'please type search word...'
 	@people = Array.new
 	  if request.post? then
-	  	@people = Person.where name: params['find']
+	  	@people = Person.where "age >= ?", params[:find]
 	  end
   end
   
